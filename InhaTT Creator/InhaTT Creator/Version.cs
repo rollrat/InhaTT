91491c3f0ac6cbfb5b2f9c65c688901e66ab1f15
HyunJun Jeong <rollrat@naver.com>
2017-07-04 22:28:03 +0900

    ver

--- InhaTT Creator/InhaTT Creator/Version.cs
+++ InhaTT Creator/InhaTT Creator/Version.cs
@@ -10,7 +10,7 @@ namespace InhaTT_Creator
 {
     class Version
     {
-        static public string Text = "0.2.3.1";
+        static public string Text = "0.2.4";
         static public string Name = "InhaTimeTable Creator";
     }
 }
